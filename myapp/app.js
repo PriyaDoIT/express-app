dfd5f5b9d611fdfe6488686dd7734f4b84c4dc05
pshah824 <pshah824@gmail.com>
2018-02-21 11:11:24 -0600

    app.js

---
+++ myapp/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000,()=> console.log('Example app listening on port 3000!'))
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/',  (req, res) => res.send('Got a POST request'))
+
+app.put('/user',  (req, res) => res.send('Got a PUT request at /user'))
+
+app.delete('/user', (req, res) => res.send('Got a DELETE request at /user'))
+  
